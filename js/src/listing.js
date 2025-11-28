1b8b46b55083f747fa4fe07291b332e2e2b54267
Marco Sabatini <marsab04188@stud.noroff.no>
2025-11-28 11:49:38 +0100

    several changes on listing.html. login hide/display, list feed organization, addressing path to correct page, addressing nav links, addressing create-bid button. index.html adjustments to follow listing logistic

--- js/src/listing.js
+++ js/src/listing.js
@@ -1,6 +1,26 @@
 import { renderFeed } from "../api/renderList.js";
 import { apiGet } from "../api/api.js";
-import { API_BASE } from "../utils/constants.js";
+import { getUser , deleteUser } from "../storage/local.js";
+
+document.addEventListener("DOMContentLoaded", ()=> {
+    const user = getUser();
+
+    const elementsToToggle = [
+        {id: 'sign-in_text', showIf: !user},
+        { id: 'login-link', showIf: !user},
+        { id: 'create-link', showIf: !!user},
+        {id : 'profile-div', showIf: !!user },
+        {id: 'logout-btn', showIf: !!user},
+        {id: 'login-btn', showIf: !user},
+    ];
+
+    elementsToToggle.forEach(({id, showIf}) =>  {
+        const element = document.getElementById(id);
+        element.style.display = showIf? "block" : "none";
+    })
+});
+
+
 
 async function listFeed() {
     const auctionUrl = '/auction/listings';
@@ -14,3 +34,21 @@ async function listFeed() {
 }
 
 listFeed();
+
+// BUTTONS FUNCTIONALITIES
+// logout btn
+const logOutBtn = document.getElementById('logout-btn');
+
+logOutBtn.addEventListener("click", async ()=> {
+
+    deleteUser();
+    window.location.href = '../index.html';
+    alert("You are being redirected to the Main Page");
+});
+
+const loginLink = document.getElementById('login-link');
+
+loginLink.addEventListener("click", async ()=> {
+    window.location.href = 'login.html';
+    alert("You are being redirected to the Sign In page")
+})
